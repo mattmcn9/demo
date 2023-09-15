b7384f3943b6ef8f8acc4140c5962b0762fd032f
mattmcn9 <mattmcn9@gmail.com>
2023-09-15 16:18:39 -0400

    fixed indentation and added icons

--- src/main/java/com/example/demo/FileDisplay.java
+++ src/main/java/com/example/demo/FileDisplay.java
@@ -4,19 +4,31 @@ import java.io.File;
 
 public class FileDisplay {
     private final File file;
-    private final String displayName;
+    private final int depth;
 
-    public FileDisplay(File file, String displayName) {
+    public FileDisplay(File file, int depth) {
         this.file = file;
-        this.displayName = displayName;
+        this.depth = depth;
     }
 
     public File getFile() {
         return file;
     }
 
+    public int getDepth() {
+        return depth;
+    }
+
+    private String getIndentation() {
+        StringBuilder indent = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            indent.append("--");
+        }
+        return indent.toString();
+    }
+
     @Override
     public String toString() {
-        return displayName;
+        return getIndentation() + file.getName();
     }
 }
--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -1,32 +1,29 @@
 package com.example.demo;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class HelloApplication extends Application {
+    ComboBox<FileDisplay> folderComboBox = new ComboBox<>();
 
+    ListView<String> folderContentsListView = new ListView<>();
     @Override
     public void start(Stage primaryStage) {
-
-
-        ComboBox<FileDisplay> folderComboBox = new ComboBox<>();
-
-        ListView<String> folderContentsListView = new ListView<>();
-
-
 
         // Let's use the current directory for this example
         File currentDirectory = new File(System.getProperty("user.dir"));
@@ -37,50 +34,51 @@ public class HelloApplication extends Application {
         folderComboBox.setButtonCell(new ListCell<FileDisplay>() {
             private final ImageView folderIcon = new ImageView(new Image("D:/Projects/JavaFX/JavaFx-playground/demo/src/main/java/com/example/demo/folderIcon.png"));
 
-            private final ImageView fileIcon = new ImageView(new Image("D:/Projects/JavaFX/JavaFx-playground/demo/src/main/java/com/example/demo/Images/fileIcon.png"));
-
             @Override
             protected void updateItem(FileDisplay item, boolean empty) {
                 super.updateItem(item, empty);
-                folderIcon.setFitWidth(10);
-                folderIcon.setFitHeight(10);
-                fileIcon.setFitWidth(10);
-                fileIcon.setFitHeight(10);
                 if (item == null || empty) {
                     setText(null);
                     setGraphic(null);
                 } else {
-                    setText(item.toString().replaceAll("--", ""));
-                    setGraphic(folderIcon);
+                    HBox hbox = new HBox(2);  // 2 is the spacing between elements
+                    folderIcon.setFitWidth(10);
+                    folderIcon.setFitHeight(10);
+                    Label indentLabel = new Label(item.toString().replaceAll("--", ""));  // Remove the '--' for the selected display
+                    hbox.getChildren().addAll(folderIcon, indentLabel);
+
+                    setText(null);  // We set the text to null because we're using the graphic for display
+                    setGraphic(hbox);
                 }
             }
         });
+
 
 
         // Custom display for dropdown items (with "--")
         folderComboBox.setCellFactory(lv -> new ListCell<FileDisplay>() {
-
             private final ImageView folderIcon = new ImageView(new Image("D:/Projects/JavaFX/JavaFx-playground/demo/src/main/java/com/example/demo/folderIcon.png"));
-
-            private final ImageView fileIcon = new ImageView(new Image("D:/Projects/JavaFX/JavaFx-playground/demo/src/main/java/com/example/demo/Images/fileIcon.png"));
 
             @Override
             protected void updateItem(FileDisplay item, boolean empty) {
                 super.updateItem(item, empty);
-                folderIcon.setFitWidth(10);
-                folderIcon.setFitHeight(10);
-                fileIcon.setFitWidth(10);
-                fileIcon.setFitHeight(10);
                 if (empty || item == null) {
                     setText(null);
                     setGraphic(null);
                 } else {
-                    setText(item.toString());
-                    setGraphic(folderIcon);  // As the ComboBox only lists directories, we only use the folder icon.
+                    HBox hbox = new HBox(2);
+                    folderIcon.setFitWidth(10);
+                    folderIcon.setFitHeight(10);
+                    Label indentLabel = new Label(getIndentation(item.getDepth()));  // Use the helper method here
+                    Label folderLabel = new Label(item.getFile().getName());
+                    //hbox.setAlignment(Pos.CENTER);
+                    hbox.getChildren().addAll(indentLabel, folderIcon, folderLabel);
+
+                    setText(null);
+                    setGraphic(hbox);
                 }
             }
         });
-
 
         folderContentsListView.setCellFactory(lv -> new ListCell<String>() {
             private final ImageView folderIcon = new ImageView(new Image("D:/Projects/JavaFX/JavaFx-playground/demo/src/main/java/com/example/demo/folderIcon.png"));
@@ -112,25 +110,11 @@ public class HelloApplication extends Application {
         });
 
         folderComboBox.getSelectionModel().selectLast();
+        updateContentsListView(folderComboBox.getItems().get(folderComboBox.getItems().size() -1));
 
         // Listen for changes in the ComboBox's selected item
         folderComboBox.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
-            File selectedDir = newVal.getFile();
-
-            // Update the ListView with the contents of the selected directory
-            if (selectedDir.isDirectory()) {
-                File[] files = selectedDir.listFiles(file -> !file.isHidden());
-                if (files != null) {
-                    List<String> fileNames = Arrays.stream(files)
-                            .map(File::getName)
-                            .collect(Collectors.toList());
-                    folderContentsListView.getItems().setAll(fileNames);
-                } else {
-                    folderContentsListView.getItems().clear();
-                }
-            } else {
-                folderContentsListView.getItems().clear();
-            }
+            updateContentsListView(newVal);
         });
 
 
@@ -141,6 +125,36 @@ public class HelloApplication extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+    private void updateContentsListView(FileDisplay newVal){
+        File selectedDir = newVal.getFile();
+
+        // Update the ListView with the contents of the selected directory
+        if (selectedDir.isDirectory()) {
+            File[] files = selectedDir.listFiles(file -> !file.isHidden());
+            if (files != null) {
+                List<String> fileNames = Arrays.stream(files)
+                        .map(File::getName)
+                        .collect(Collectors.toList());
+                folderContentsListView.getItems().setAll(fileNames);
+                folderContentsListView.getItems().setAll(fileNames);
+            } else {
+                folderContentsListView.getItems().clear();
+            }
+        } else {
+            folderContentsListView.getItems().clear();
+        }
+
+    }
+
+    private String getIndentation(int depth) {
+        StringBuilder indent = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            indent.append("--");
+        }
+        return indent.toString();
+    }
+
 
     private void populateComboBox(File currentDirectory, ComboBox<FileDisplay> comboBox) {
         File dir = currentDirectory;
@@ -163,12 +177,10 @@ public class HelloApplication extends Application {
                 displayName = "/";
             }
 
-            comboBox.getItems().add(0, new FileDisplay(dir, repeatString("--", depth) + displayName));
+            comboBox.getItems().add(0, new FileDisplay(dir, depth));
             dir = dir.getParentFile();
         }
     }
-
-
     private String repeatString(String str, int times) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < times; i++) {
@@ -181,7 +193,3 @@ public class HelloApplication extends Application {
         launch(args);
     }
 }
-
-
-
-
